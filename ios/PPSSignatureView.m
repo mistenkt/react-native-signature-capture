7ee157e2d6d3dd5ad0e994edf884ab89c9337856
ajhenri <amandajohe@live.com>
2018-02-27 11:19:55 -0500

    decrease size of maximum stroke width

--- ios/PPSSignatureView.m
+++ ios/PPSSignatureView.m
@@ -3,7 +3,7 @@
 #import "RSSignatureViewManager.h"
 
 #define             STROKE_WIDTH_MIN 0.004 // Stroke width determined by touch velocity
-#define             STROKE_WIDTH_MAX 0.030
+#define             STROKE_WIDTH_MAX 0.010
 #define       STROKE_WIDTH_SMOOTHING 0.5   // Low pass filter alpha
 
 #define           VELOCITY_CLAMP_MIN 20
